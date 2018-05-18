6c6244314d8446c353253fdcb5b6c5852c6020a6
corsa-dn <linkjkeee@gmail.com>
2018-05-18 14:08:17 +0300

    Added Pug compiler; plumber set as error handler by default+notify

--- gulpfile.js
+++ gulpfile.js
@@ -6,39 +6,61 @@ var gulp = require('gulp'),
 	imagemin = require('gulp-imagemin'),
 	pngquant = require('imagemin-pngquant'),
 	plumber = require('gulp-plumber'),
-	babel = require('gulp-babel');
+	babel = require('gulp-babel'),
+	pug = require('gulp-pug'),
+	notify = require('gulp-notify');
 
-gulp.task('scss', function() {
+gulp.task('pug', function () {
+	return gulp.src('./src/views/*.pug')
+		.pipe(plumber({
+			errorHandler: notify.onError()
+		}))
+		.pipe(pug({
+			pretty: true
+		}))
+		.pipe(gulp.dest('./dist/'))
+		.pipe(browserSync.stream());
+})
+
+gulp.task('scss', function () {
 	return gulp.src('./src/scss/*.scss')
-				.pipe(sass())
-				.pipe(prefixer({browsers: ['last 3 versions']}))
-				.pipe(csso())
-				.pipe(gulp.dest('./dist/css/'));
-	});
+		.pipe(plumber({
+			errorHandler: notify.onError()
+		}))
+		.pipe(sass())
+		.pipe(prefixer({
+			browsers: ['last 3 versions']
+		}))
+		.pipe(csso())
+		.pipe(gulp.dest('./dist/css/'));
+});
 
-gulp.task('image-o', function() {
+gulp.task('image-o', function () {
 	gulp.src('./src/img/*.*')
 		.pipe(imagemin())
 		.pipe(gulp.dest('./dist/img/'));
 });
 
-gulp.task('babel', function() {
+gulp.task('babel', function () {
 	gulp.src('./src/js/*.js')
+		.pipe(plumber({
+			errorHandler: notify.onError()
+		}))
 		.pipe(babel({
 			presets: ['@babel/env']
 		}))
 		.pipe(gulp.dest('./dist/js/'));
 });
 
-gulp.task('dev', function() {
-  	browserSync.init(['./dist/css/*.css', './dist/*.html', './dist/js/*.js'], {
-	    server: {
-	    	baseDir: './dist/'
-	    }
+gulp.task('dev', function () {
+	browserSync.init(['./src/views/**/*.pug', './dist/css/*.css', './dist/*.html', './dist/js/*.js'], {
+		server: {
+			baseDir: './dist/'
+		}
 	});
 
-  gulp.watch(['./src/scss/*.scss'], ['scss']);
-  gulp.watch(['./src/img/*.*'], ['image-o']);
-  gulp.watch(['./src/js/*.js'], ['babel']);
+	gulp.watch(['./src/views/**/*.pug'], ['pug']);
+	gulp.watch(['./src/scss/*.scss'], ['scss']);
+	gulp.watch(['./src/img/*.*'], ['image-o']);
+	gulp.watch(['./src/js/*.js'], ['babel']);
 });
-
